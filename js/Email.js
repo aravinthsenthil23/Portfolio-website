91b1e16910d11b5e226ad68e90d2d8b9fc8e1f47
aravinthsenthil23 <senthilaravinth0110@gmail.com>
2026-05-12 16:57:38 +0530

    Update Email.js

--- js/Email.js
+++ js/Email.js
@@ -1,4 +1,4 @@
 
-const emailServiceID = 'service_3w7o98r';      // Paste your service ID
+const emailServiceID = 'service_exwa3so';      // Paste your service ID
 const emailTemplateID = 'template_bzb5y04';   // Paste your template ID
 const emailUserID = 'WEq5a-Oauowz0tQe0';                 // Paste your public key
